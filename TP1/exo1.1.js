6562efc8c555ee0cb3f6fa2af5fb2b38ee433c9b
FlorianMMI <florian.bounissou@etu.unlim.fr>
2024-11-04 15:47:42 +0100

    first commit

---
+++ TP1/exo1.1.js
@@ -0,0 +1,13 @@
+function setup() {
+    // ?
+    createCanvas(windowWidth, windowHeight);
+    // ?
+    background(100);
+ }
+ 
+ 
+ function draw() {
+    // ?
+    circle(mouseX, mouseY, 20);
+ }
+ 
