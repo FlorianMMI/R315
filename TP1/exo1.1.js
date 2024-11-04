00e645acd6de014846c53eab481c5a9d9f9d5551
FlorianMMI <florian.bounissou@etu.unlim.fr>
2024-11-04 16:04:01 +0100

    Commit b et C

--- TP1/exo1.1.js
+++ TP1/exo1.1.js
@@ -1,13 +1,57 @@
-function setup() {
-    // ?
-    createCanvas(windowWidth, windowHeight);
-    // ?
-    background(100);
- }
+/**
+ * Exercice 1.1 : a
+ */
+
+// function setup() {
+//     // Crée un canvas de la taille de la fenêtre
+//     createCanvas(windowWidth, windowHeight);
+//     // Met la valeur de fond à 100
+//     background(100);
+//  }
  
  
- function draw() {
-    // ?
-    circle(mouseX, mouseY, 20);
- }
+//  function draw() {
+//     // Crée un cercle de la taille 20 à la position de la souris
+//     circle(mouseX, mouseY, 20);
+//  }
  
+
+/**
+ * Exercice 1.1 : b
+**/
+
+// function setup() {
+//         // Crée un canvas de la taille de la fenêtre
+//         createCanvas(400, 400);
+//         // Met la valeur de fond à 100
+//         background(0,0,255);
+//      }
+     
+     
+//      function draw() {
+//         // Crée un cercle de la taille 20 à la position de la souris
+//         circle(mouseX, mouseY, random(10, 100));
+//     }
+
+
+
+/**
+ * Exercice 1.1 : c
+ * Réponse : le cercle est toujours visible, le background n'est pas visible et la canva ne s'applique pas à la fenêtre
+ */
+
+// function setup() {
+//     // Met la valeur de fond à 100
+//     background(100);
+
+//     // Crée un canvas de la taille de la fenêtre
+//     createCanvas(windowWidth, windowHeight);
+    
+//  }
+ 
+ 
+//  function draw() {
+//     // Crée un cercle de la taille 20 à la position de la souris
+//     circle(mouseX, mouseY, 20);
+//  }
+
